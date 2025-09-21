de4e25111c49f314d2451aad16a820f040a2ccfb
prlgadminuser <liam.heizmann@gmail.com>
2025-09-21 15:29:06 +0200

    Delete ENV.js

--- ENV.js
+++
@@ -1,4 +0,0 @@
-// Environment configuration for the shop server
-module.exports = {
-  MONGO_URI: "mongodb+srv://sr-server-user:I8u8a8iOBNkunxRK@cluster0.ed4zami.mongodb.net/?retryWrites=true&w=majority"
-};
