e2f7a4631c896775fb5e7c1cd99b9372d26fcbd4
prlgadminuser <liam.heizmann@gmail.com>
2026-01-15 14:53:45 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,7 +5,8 @@ const globalConfig = {
   updateShopOnServerStart:  true,
   dailyRotationDiscountsAmount: [1, 2], // number (min, max) of offers which receive a discount
   dailyRotationDiscountRates: [20, 30], // number (min, max) how much % discount each discounted offer gets
-  dailyRotationitemPrefixesSelection: ["HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE"]
+  dailyRotationitemPrefixesSelection: ["HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE", "HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE"]
 }
+
 
 module.exports = { globalConfig }
