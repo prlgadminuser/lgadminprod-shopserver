6d9d6df9c60b6d1a3ca24e414085106d72b92087
prlgadminuser <liam.heizmann@gmail.com>
2026-01-18 18:29:34 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,12 +3,13 @@
 
 const globalConfig = {
   updateShopOnServerStart:  true,
-  dailyRotationDiscountsAmount: [10, 12], // number (min, max) of offers which receive a discount
+  dailyRotationDiscountsAmount: [1, 1], // number (min, max) of offers which receive a discount
   dailyRotationDiscountRates: [20, 30], // number (min, max) how much % discount each discounted offer gets
-  dailyRotationitemPrefixesSelection: ["HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE","HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE","HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE"]
+  dailyRotationitemPrefixesSelection: ["HAT", "TOP", "HAT", "TOP", "HAT", "TOP", "BANNER", "POSE"]
 }
 
 
 module.exports = { globalConfig }
+
 
 
